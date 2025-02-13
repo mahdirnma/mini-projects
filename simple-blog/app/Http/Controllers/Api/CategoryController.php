b2537f8f8801009d341867b52e023b4bf0311529
mahdirnma <rahnamamahdi78@gmail.com>
2025-02-14 01:15:15 +0330

    create api for add post

--- simple-blog/app/Http/Controllers/Api/CategoryController.php
+++ simple-blog/app/Http/Controllers/Api/CategoryController.php
@@ -3,40 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Api\StorePostRequest;
 use App\Models\Category;
-use App\Models\Post;
 use Illuminate\Http\Request;
 
-class PostController extends Controller
+class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $posts = Post::paginate(2);
-        return response()->json([
-            'data' => $posts
-        ]);
+        //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(Request $request)
     {
-        $post=Post::create($request->all());
-        return response()->json([
-            'message'=>'Post created successfully',
-            'data' => $post
-        ]);
+        //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
+    public function show(Category $category)
     {
         //
     }
@@ -44,7 +35,7 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Post $post)
+    public function update(Request $request, Category $category)
     {
         //
     }
@@ -52,7 +43,7 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function destroy(Category $category)
     {
         //
     }
---
+++ simple-blog/app/Http/Requests/Api/StorePostRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|max:255',
+            'description' => 'required|max:255',
+            'category_id' => 'required|exists:categories,id',
+
+        ];
+    }
+
+    public function failedValidation(Validator $validator): void
+    {
+        abort(response()->json([
+            'message'=>'validation error',
+        ],422));
+    }
+}
