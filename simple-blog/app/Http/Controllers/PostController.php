2f4beb3ffe9f4de2171df8940c5451387127e3b3
mahdirnma <rahnamamahdi78@gmail.com>
2025-02-13 23:30:22 +0330

    create delete post process for admin

--- simple-blog/app/Http/Controllers/PostController.php
+++ simple-blog/app/Http/Controllers/PostController.php
@@ -76,6 +76,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return to_route('posts.index');
     }
 }
