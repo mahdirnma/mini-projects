c144142dbe3d4a9df52ae1272575ca4c0945f9f3
mahdirnma <rahnamamahdi78@gmail.com>
2025-02-13 19:25:08 +0330

    fill models and create relations

--- simple-blog/app/Models/Post.php
+++ simple-blog/app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'category_id',
+    ];
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
