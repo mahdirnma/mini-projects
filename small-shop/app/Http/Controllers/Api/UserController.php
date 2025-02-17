641e28971a274e27c01bc7ce9104895fe1e2be94
mahdirnma <rahnamamahdi78@gmail.com>
2025-02-17 17:59:59 +0330

    create api for show user

--- small-shop/app/Http/Controllers/Api/UserController.php
+++ small-shop/app/Http/Controllers/Api/UserController.php
@@ -34,7 +34,7 @@ class UserController extends ApiController
      */
     public function show(User $user)
     {
-        //
+        return $this->response(new UserApiResource($user), 200);
     }
 
     /**
